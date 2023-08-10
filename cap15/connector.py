03bdcc43c41c5c735f4b67eb68a9721a35189102
Enrique Avila <senquevila@gmail.com>
2023-08-09 18:21:26 -0600

    Cap 15

--- cap15/connector.py
+++ cap15/connector.py
@@ -2,6 +2,6 @@ import sqlite3
 
 conn = sqlite3.connect('database.db')
 cur = conn.cursor()
-cur.execute('DROP TABLE Canciones')
-cur.execute('CREATE TABLE Canciones (titulo TEXT NOT NULL, reproducciones INTEGER)')
+cur.execute('DROP TABLE IF EXISTS Canciones')
+cur.execute('CREATE TABLE Canciones (titulo TEXT NOT NULL UNIQUE, reproducciones INTEGER DEFAULT 0)')
 conn.close()
---
+++ cap15/crud.py
@@ -0,0 +1,36 @@
+'''
+C -create
+R - read
+U - update
+D - delete
+'''
+
+import sqlite3
+conn = sqlite3.connect('database.db')
+cur = conn.cursor()
+
+# create
+cur.execute('INSERT INTO Canciones (titulo, reproducciones) VALUES (?, ?)',
+('Thunderstruck', 20))
+cur.execute('INSERT INTO Canciones (titulo, reproducciones) VALUES (?, ?)',
+('My Way', 15))
+conn.commit()
+
+# update
+cur.execute("UPDATE Canciones SET reproducciones = ? WHERE titulo = ?", (50, "Prueba"))
+conn.commit()
+
+
+print("Delete...")
+#cur.execute('DELETE FROM Canciones WHERE reproducciones < 100')
+#conn.commit()
+
+
+# read
+print('Canciones:')
+cur.execute('SELECT titulo, reproducciones FROM Canciones')
+for fila in cur:
+    print(fila)
+
+
+cur.close()
